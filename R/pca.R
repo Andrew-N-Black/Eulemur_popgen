8baac77b479318b0abfd40f36e47f584633d31b5
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-12-12 09:54:44 -0800

    Update pca.R

--- R/pca.R
+++ R/pca.R
@@ -1,20 +1,18 @@
-
 library(ggplot2)
-library(RColorBrewer)
 
 #Read in metadata
 Fecalseq_metadata_v4 <- read.csv("~/Fecalseq_metadata_v4.csv")
-#Set factor
-#sample_pop_sites$Pop <- factor(sample_pop_sites$Pop, levels = c("OREG","CCAL", "INYO","SCAL"))
+#Import covariation matrix
 cov<-as.matrix(read.table("revised.cov"))
+#Extract eigenvalues
 axes<-eigen(cov)
+#Calculate percent variation for the first six axes for plotting purposes
 head(axes$values/sum(axes$values)*100)
-[1] 33.750273  6.073139  2.713775  2.577800  2.459278  2.393372
+#[1] 33.750273  6.073139  2.713775  2.577800  2.459278  2.393372
 
+#Extract the first three axes
 PC1_3<-as.data.frame(axes$vectors[,1:3])
-title1<-"Locality"
-title2<-"Assumed Ancestry"
 
-#By Population
+#Plot using ggplot2
 ggplot(data=PC1_3, aes(y=V2, x=V1))+geom_point(size=5,pch=21,aes(fill=Fecalseq_metadata_v4$Putative_species))+ theme_classic() + xlab("PC1 (33.75%)") +ylab("PC2 (6.07%)")+geom_hline(yintercept=0,linetype="dashed")+geom_vline(xintercept =0,linetype="dashed") +scale_fill_manual(values=c("orchid","dodgerblue2","yellow"))+theme(legend.title=element_blank())
 ggsave("~/pca.lemur.svg")
