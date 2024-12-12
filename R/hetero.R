11be28abd711ce2535c4a4ed61b6f5d77392355d
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-12-12 09:52:18 -0800

    Update hetero.R

--- R/hetero.R
+++ R/hetero.R
@@ -1,11 +1,9 @@
-
-
 library(ggplot2)
-library(RColorBrewer)
 
 #Read in metadata with individual heterozygosity
 Fecalseq_metadata_v4 <- read.csv("~/Fecalseq_metadata_v4.csv")
+#Change the order of groups before plotting
 Fecalseq_metadata_v4$Putative_species <- factor(Fecalseq_metadata_v4$Putative_species, levels = c("E_rufifrons","Hybrid", "E_cinereiceps"))
 
-#Color by region
+#Color by putative species identity
 ggplot(Fecalseq_metadata_v4,aes(x=Putative_species,y=H,fill=Putative_species))+geom_boxplot(show.legend =FALSE)+xlab("")+ylab("Individual Heterozygosity")+theme_classic()+theme_classic()+scale_fill_manual(values=c("orchid","dodgerblue2","yellow"))+theme(legend.position="none")
