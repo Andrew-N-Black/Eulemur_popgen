e49ff23dc93f4126c263d84b1bf16c67c3d4f721
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-12-12 10:11:36 -0800

    Update angsd.sh

--- analysis/angsd.sh
+++ analysis/angsd.sh
@@ -1,3 +1,5 @@
+#Below was the SLURM script used to generate genotype likelihoods with Angsd
+
 #!/bin/bash
 #SBATCH -A core
 #SBATCH -p core 
@@ -9,6 +11,6 @@
 REF=/nfs7/IB/Everson_Lab/BLACK/POP/raw/GCF_041146395.1_OSU_ERuf_1_genomic.fna 
 
 #Generate angsd output files for all OK regions
-angsd -P 8 -out ../angsd_out/mapQ20Q20pvalue \
--bam ./ALL_bamlist -doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 4 -doMaf 1 -minMaf 0.05 -rmTriallelic 1  \
--minInd 49 -uniqueOnly 1 -minMapQ 20 -minQ 20 -SNP_pval 1e-6  -ref $REF -anc $REF
+angsd -P 8 -out ../angsd_out/revised \
+-bam ./revised_bamlist.txt -doCounts 1 -GL 1 -doGlf 2 -doMajorMinor 4 -doMaf 1 -minMaf 0.05 -rmTriallelic 1  \
+-minInd 36 -minMapQ 20 -minQ 20 -SNP_pval 1e-6  -ref $REF -anc $REF
