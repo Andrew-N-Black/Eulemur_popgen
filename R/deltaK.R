d0f7d56fb6588a26ac929a556072f6a4b39bf6fd
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-12-12 09:51:22 -0800

    Update deltaK.R

--- R/deltaK.R
+++ R/deltaK.R
@@ -1,5 +1,6 @@
 library(ggplot2)
-
+#Delta K values were obtained from the following web server: https://clumpak.tau.ac.il/ and added to a text file:
 delta_lemur <- read.csv("~/delta_lemur.txt", sep="")
+#And plotted using the following command
 ggplot(delta_lemur,aes(x=K,y=Delta))+geom_line(color="black") + geom_point(color="grey22",size=6)+theme_classic()+ylab("∆K")
 ggsave("lemur_deltaK.svg")
