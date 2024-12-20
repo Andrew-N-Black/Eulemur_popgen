6362209590edbdeb0d364f3b7d836e854c3ea007
Andrew-N-Black <63075313+Andrew-N-Black@users.noreply.github.com>
2024-12-20 11:28:48 -0800

    Update admix.sh

--- analysis/admix.sh
+++ analysis/admix.sh
@@ -16,8 +16,24 @@ do
         do
                  ~/Software/NGSadmix -P 8  \
                 -K ${i} -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
-                -likes revised.beagle.gz \
-                -outfiles  ADX_R/R_${j}/K${i}_R${j}
+                -likes revisedMin3Maf025.beagle.gz \
+                -outfiles  ADX_R2/R_${j}/K${i}_R${j}
+                i=$[$i+1]
+
+        done
+	j=$[$j+1]
+done
+
+#Repeat using three individuals removed
+#for  ((i=1;i<=10;i++));  do mkdir R_$i; done
+while [ $j -lt 100 ]
+do
+  	for i in {1..10}
+        do
+                 ~/Software/NGSadmix -P 8  \
+                -K ${i} -maxiter 50000 -tol 1e-9 -tolLike50 1e-9 \
+                -likes revisedMin3Maf025Sub.beagle.gz \
+                -outfiles  ADX_R3/R_${j}/K${i}_R${j}
                 i=$[$i+1]
 
         done
